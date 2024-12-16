77f2301b93c21599ea322939670e22ad2aac3a39
erika.duarte <erika.duarte.mora@gmail.com>
2024-12-16 17:02:18 -0500

    feat(room): add attributes, constructor, getter, and setter

--- src/main/java/org/example/entities/Room.java
+++ src/main/java/org/example/entities/Room.java
@@ -1,4 +1,57 @@
 package org.example.entities;
 
 public class Room {
+   private String name;
+   private String description;
+   private int maximumCapacity;
+   private double price;
+   private int quantity;
+
+    public Room(String name, String description, int maximumCapacity, double price, int quantity) {
+        this.name = name;
+        this.description = description;
+        this.maximumCapacity = maximumCapacity;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getMaximumCapacity() {
+        return maximumCapacity;
+    }
+
+    public void setMaximumCapacity(int maximumCapacity) {
+        this.maximumCapacity = maximumCapacity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
