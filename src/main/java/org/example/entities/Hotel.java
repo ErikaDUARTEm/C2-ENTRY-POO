c57c9c22e7af59982a428598fdc1ce74ae2d8149
erika.duarte <erika.duarte.mora@gmail.com>
2024-12-16 20:53:07 -0500

    feat(hotel): add class hotel

--- src/main/java/org/example/entities/Hotel.java
+++ src/main/java/org/example/entities/Hotel.java
@@ -1,5 +1,20 @@
 package org.example.entities;
 
-public class Hotel extends Accommodation {
+import java.util.List;
 
+public class Hotel extends Accommodation {
+   protected List<Room> rooms;
+
+    public Hotel(String type, String name, String city, String description, double rating, double basePrice, double totalPrice, int availableCheckInDate, int availableCheckOutDate, int totalRooms, int numberOfConfirmedRooms, List<Room> availableRooms, List<Room> rooms) {
+        super(type, name, city, description, rating, basePrice, totalPrice, availableCheckInDate, availableCheckOutDate, totalRooms, numberOfConfirmedRooms, availableRooms);
+        this.rooms = rooms;
+    }
+
+    public List<Room> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(List<Room> rooms) {
+        this.rooms = rooms;
+    }
 }
